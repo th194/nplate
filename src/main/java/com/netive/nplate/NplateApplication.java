73728d98544942cdb8354443cd9c17f440c31cd3
th194 <th194@naver.com>
2022-10-19 09:34:19 +0900

    NETIVE_PROJECT01

---
+++ src/main/java/com/netive/nplate/NplateApplication.java
@@ -0,0 +1,13 @@
+package com.netive.nplate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NplateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NplateApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/netive/nplate/NplateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.netive.nplate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NplateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
