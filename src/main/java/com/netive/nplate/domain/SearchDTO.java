60fc7528003781c0fea6eb73b8de27fae3e1baf1
th194 <th194@naver.com>
2022-11-08 13:27:32 +0900

    [게시판] 검색 유형 삭제 (임시 주석처리)

--- src/main/java/com/netive/nplate/domain/SearchDTO.java
+++ src/main/java/com/netive/nplate/domain/SearchDTO.java
@@ -12,7 +12,7 @@ public class SearchDTO {
     private int recordSize;             // 페이지당 출력할 데이터 개수
     private int pageSize;               // 화면 하단에 출력할 페이지 사이즈
     private String keyword;             // 검색 키워드
-    private String searchType;          // 검색 유형
+//    private String searchType;          // 검색 유형
     private String memberId;            // 회원아이디
     private Pagination pagination;      // 페이지네이션 정보
 
