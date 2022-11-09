0d2094c49fdb548eeb34ff004f06f56e8d872ac1
th194 <th194@naver.com>
2022-11-09 16:52:38 +0900

    [게시판] 이미지파일 테이블 작업중

---
+++ src/main/java/com/netive/nplate/mapper/BoardFileMapper.java
@@ -0,0 +1,25 @@
+package com.netive.nplate.mapper;
+
+import com.google.gson.JsonArray;
+import com.netive.nplate.domain.BoardFileDTO;
+import org.apache.ibatis.annotations.Mapper;
+
+import java.util.List;
+
+@Mapper
+public interface BoardFileMapper {
+
+    // 게시판 파일 저장
+    public int registerBoardFile(BoardFileDTO dto);
+
+    // 게시판 파일 목록
+    public List<BoardFileDTO> selectBoardFile(Long idx);
+
+    public JsonArray selectBoardFileJson(Long idx);
+
+    // 게시판 파일 수정
+    public int updateBoardFile(BoardFileDTO dto);
+
+    // 게시판 파일 삭제
+    public int deleteBoardFile(Long idx);
+}
