aa0468982b908463890fa1aaac7bc1fd36592723
th194 <th194@naver.com>
2022-11-11 18:22:33 +0900

    [게시판] 댓글 수정중

--- src/main/java/com/netive/nplate/domain/ReplyDTO.java
+++ src/main/java/com/netive/nplate/domain/ReplyDTO.java
@@ -2,12 +2,17 @@ package com.netive.nplate.domain;
 
 import lombok.Getter;
 import lombok.Setter;
+import lombok.ToString;
 
 @Getter
 @Setter
+@ToString
 public class ReplyDTO {
     private Long answerNo;                         // 댓글 번호 PK
     private Long bbscttNo;                         // 게시글 번호 FK
+    private int answerGroup;                       // 댓글이 속한 댓글 번호      --> 대댓글용
+    private int answerGroupDp;                     // 댓글의 깊이 ( 모댓글이면 0, 답글이면 1 ) --> 대댓글용
+    private String answerDeleteAt;                 // 댓글 삭제 여부
     private String answerCn;                       // 댓글 내용
     private String answerWrter;                    // 작성자
     private String answerRgsde;                    // 등록일
--- src/main/java/com/netive/nplate/service/ReplyService.java
+++ src/main/java/com/netive/nplate/service/ReplyService.java
@@ -2,11 +2,14 @@ package com.netive.nplate.service;
 
 import com.netive.nplate.domain.ReplyDTO;
 
+import javax.servlet.http.HttpServletRequest;
 import java.util.List;
 
 public interface ReplyService {
-    // 댓글 등록/수정
+    // 댓글 등록
     public boolean registerReply(ReplyDTO params);
+    // 댓글 수정
+    public boolean updateReply(ReplyDTO params);
     // 댓글 삭제
     public boolean deleteReply(Long idx);
     // 댓글 목록
