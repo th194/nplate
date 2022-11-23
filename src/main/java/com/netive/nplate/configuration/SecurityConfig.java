7070c8cead1b1a251317d1adb2c5b54a43d20f20
th194 <th194@naver.com>
2022-11-23 14:20:45 +0900

    [게시판] 스프링 시큐리티 스마트에디터 로드 불가 해결

--- src/main/java/com/netive/nplate/configuration/SecurityConfig.java
+++ src/main/java/com/netive/nplate/configuration/SecurityConfig.java
@@ -3,6 +3,7 @@ package com.netive.nplate.configuration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfiguration;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
@@ -25,6 +26,9 @@ public class SecurityConfig {
                 .defaultSuccessUrl("/login/success")
                 .failureUrl("/login/fail")
                 .permitAll();
+
+        // 스프링 시큐리티 적용 시 스마트에디터 로드 불가 >> 해결
+        http.headers().frameOptions().sameOrigin();
         return http.build();
     }
 
@@ -32,4 +36,5 @@ public class SecurityConfig {
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
     }
+
 }
