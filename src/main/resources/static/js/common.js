3e69bc0f065aa7005e272913c01490b6c278be81
th194 <th194@naver.com>
2022-12-20 13:53:22 +0900

    [게시판] 모달 수정

---
+++ src/main/resources/static/js/common.js
@@ -0,0 +1,66 @@
+/* 모달 오픈 공통함수 S */
+var openModal = {
+    /* Alert */
+    alert: function(msg, okhandler) {
+        new Promise((resolve, reject) => {
+            $("#msg_popup #btn_confirm").hide();
+            $("#msg_popup #btn_alert").show();
+
+            $("#msg_popup #alert_ok").unbind();
+            $("#msg_popup .modal-body").html(msg);
+            $('#msg_popup').modal('show');
+
+            $("#msg_popup #alert_ok").click(function() {
+                $('#msg_popup').modal('hide');
+            });
+
+            $("#msg_popup").on("hidden.bs.modal", function(e) {
+                e.stopPropagation();
+                if(okhandler != null) resolve();
+                else reject();
+            });
+        }).then(okhandler).catch(function() {});
+    },
+
+    /* Confirm */
+    confirm: function(msg, yeshandler, nohandler) {
+        new Promise((resolve, reject) => {
+            var flag = false;
+            $("#msg_popup #btn_alert").hide();
+            $("#msg_popup #btn_confirm").show();
+
+            $("#msg_popup #confirm_yes").unbind();
+            $("#msg_popup #confirm_no").unbind();
+            $("#msg_popup .modal-body").html(msg);
+            $('#msg_popup').modal('show');
+
+            $('#msg_popup').on('keypress', function (e) {
+                var keycode = (e.keyCode ? e.keyCode : e.which);
+                if(keycode == '13') {
+                    flag = true;
+                    $('#msg_popup').modal('hide');
+                }
+            });
+
+            $("#msg_popup #confirm_yes").click(function() {
+                flag = true;
+            });
+            $("#msg_popup #confirm_no").click(function() {
+                flag = false;
+            });
+
+            $("#msg_popup").on("hidden.bs.modal", function(e) {
+                e.stopPropagation();
+                if(yeshandler != null && flag == true) resolve(1);
+                else if(nohandler != null && flag == false) resolve(2);
+                else reject();
+            });
+
+        }).then(function(value) {
+            if(value == 1)      yeshandler();
+            else if(value == 2) nohandler();
+        }).catch(function() {});
+    },
+}
+
+/* 모달 오픈 공통함수 E */
